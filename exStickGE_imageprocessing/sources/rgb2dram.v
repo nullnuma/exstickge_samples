52670f57770db7a9cf8737031d1f8e8a752e7144
nullnuma <dev@nullnuma.com>
2020-05-13 08:53:16 +0900

    first commit

---
+++ exStickGE_imageprocessing/sources/rgb2dram.v
@@ -0,0 +1,108 @@
+`default_nettype none
+module rgb2dram(
+	input wire clk,
+	input wire rst,
+	//DRAM WRITE
+	output wire [32+4-1:0] data_in,//strb[35:32] + data[31:0]
+	output wire data_we,
+	output reg [32+8-1:0]ctrl_in,//len[39:32] + addr[31:0]
+	output reg ctrl_we,
+	//VIDEO
+	input wire vid_clk,
+	input wire hsync,
+	input wire vsync_n,
+	input wire de,
+	input wire [23:0] rgb_data,//red,blue,green
+	//Capture
+	input wire capture_sig,
+	output reg capture_rtn
+);
+
+	localparam WIDTH = 32'd1600;
+	wire vsync = ~vsync_n;
+
+	wire capture_de = de && capture_rtn;
+
+	wire [23:0] rgb_data_o;
+
+	assign data_in = {4'hf,rgb_data[23:16],rgb_data[7:0],rgb_data[15:8],8'hff};
+	assign data_we = capture_de;
+
+//キャプチャ
+	reg [1:0]vsync_edge;
+	always @(posedge vid_clk) begin
+		vsync_edge <= {vsync_edge[0],vsync};
+	end
+	always @(posedge clk) begin
+		if(rst)
+			capture_rtn <= 1'b0;
+		else if(vsync_edge == 2'b01)
+			capture_rtn <= (capture_sig == 1'b1);
+	end
+	
+//カウンタ
+	reg [11:0] x_cnt;
+	always @(posedge vid_clk) begin
+		if(rst)
+			x_cnt <= 12'h0;
+		else if(hsync)
+			x_cnt <= 12'h0;
+		else if(capture_de)
+			x_cnt <= x_cnt + 12'h1;
+	end
+	reg [1:0]vsync_edge;
+	always @(posedge vid_clk) begin
+		vsync_edge <= {vsync_edge[0],vsync};
+	end
+	reg [1:0]de_edge;
+	always @(posedge vid_clk) begin
+		de_edge <= {de_edge[0],capture_de};
+	end
+	reg [1:0]hsync_edge;
+	always @(posedge vid_clk) begin
+		hsync_edge <= {hsync_edge[0],hsync};
+	end
+	reg [11:0] y_cnt;
+	always @(posedge vid_clk) begin
+		if(rst)
+			y_cnt <= 12'h0;
+		else if(vsync)
+			y_cnt <= 12'h0;
+		else if(de_edge == 2'b10)
+			y_cnt <= y_cnt + 12'h1;
+	end
+	reg [7:0] write_cnt;
+	always @(posedge vid_clk) begin
+		if(rst)
+			write_cnt <= 8'h0;
+		else if(capture_de)
+			if(write_cnt < 8'd64 - 12'h1)//上限に達していなければ
+				write_cnt <= write_cnt + 8'h1;
+			else
+				write_cnt <= 8'h0;
+		else
+			write_cnt <= 8'h0;
+	end
+
+	wire [31:0] address = ((y_cnt * WIDTH) + (x_cnt - write_cnt)) * 32'h4;
+	always @(posedge vid_clk) begin
+		if(rst) begin
+			ctrl_in <= 40'h0;
+			ctrl_we <= 1'b0;
+		end else if(capture_de) begin
+			if(write_cnt == 8'd63) begin//kickするタイミング
+				ctrl_in <= {8'd64,address};
+				ctrl_we <= 1'b1;
+			end else begin
+				ctrl_we <= 1'b0;
+			end
+		end else begin
+			if(write_cnt != 8'h0) begin
+				ctrl_in <= {write_cnt+ 8'h1,address};
+				ctrl_we <= 1'b1;
+			end else begin
+				ctrl_we <= 1'b0;
+			end
+		end
+	end
+endmodule
---
+++ exStickGE_imageprocessing/tool/src/main.js
@@ -0,0 +1,154 @@
+const {
+    app,
+    Menu,
+    BrowserWindow,
+    ipcMain
+} = require('electron');
+const path = require('path');
+const url = require('url');
+const dgram = require("dgram");
+
+//IP Setting
+const recvport = 0x4000;
+const recvip = "0.0.0.0";
+let port = 0x4000;
+let ipaddr = "10.0.0.3";
+const UDPINST = { isopen: false };
+
+let mainWindow;
+
+//1度に受信するピクセル数
+const DATA_PER_PACKET = 64; //MAX 64
+const RESOLUTION_WIDTH = 1600;
+const RESOLUTION_HEIGHT = 900;
+let getnow_f = false;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // 開発ツールを有効化
+    if(process.argv.length > 2 && process.argv[2] == "debug"){
+        mainWindow.webContents.openDevTools();
+    }
+    Menu.setApplicationMenu(null);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+    watchValue(UDPINST, "isopen", (v1, v2) => {
+        console.log(`old:${v1}  new:${v2}`);
+        mainWindow.webContents.send('isopen', v2);
+    });
+}
+
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/../node_modules/electron`)
+});
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+function watchValue(obj, propName, func) {
+    let value = obj[propName];
+    Object.defineProperty(obj, propName, {
+        get: () => value,
+        set: newValue => {
+            const oldValue = value;
+            value = newValue;
+            func(oldValue, newValue);
+        },
+        configurable: true
+    });
+}
+
+const connect = (arg_ip, arg_port, arg_recvip, arg_recvport, callback) => {
+    if (UDPINST.isopen == false) {
+        UDPINST.isopen = true;
+        UDPINST.sock = dgram.createSocket("udp4");
+        UDPINST.ipaddr = arg_ip;
+        UDPINST.port = arg_port;
+        UDPINST.sock.bind(arg_recvport, arg_recvip);
+        UDPINST.sock.on('message', (message) => { callback(message); });
+        console.log("Open now");
+    } else {
+        console.log("Already Open");
+    }
+};
+
+const close = () => {
+    UDPINST.isopen = false;
+    UDPINST.sock.close();
+};
+
+const recvData = (mes) => {
+    let base = (mes[0] * 16777216 + mes[1] * 65536 + mes[2] * 256 + mes[3]) + 64;
+    mainWindow.webContents.send('recv', mes);
+    if (base < RESOLUTION_WIDTH * RESOLUTION_HEIGHT) {
+        let x = base % (RESOLUTION_WIDTH);
+        let y = parseInt(base / RESOLUTION_WIDTH);
+        console.log("next", "x", x, "y", y);
+        readReq(x, y);
+    } else {
+        //終了
+        getnow_f = false;
+        mainWindow.webContents.send('recvend');
+    }
+};
+const captureReq = () => {
+    let buf = Buffer.alloc(4);
+    buf.writeUInt32BE(0x1, 0);
+    UDPINST.sock.send(buf, 0, buf.length, UDPINST.port, UDPINST.ipaddr, (err, bytes) => {
+        if (err) throw err;
+    });
+};
+
+const readReq = (x, y) => {
+    let buf = Buffer.alloc(8);
+    buf.writeUInt32BE((((x + RESOLUTION_WIDTH * y)) << 1) | 0x0, 0);
+    buf.writeUInt32BE(DATA_PER_PACKET - 1, 4);
+    UDPINST.sock.send(buf, 0, buf.length, UDPINST.port, UDPINST.ipaddr, (err, bytes) => {
+        if (err) throw err;
+    });
+};
+
+ipcMain.on("imgreq", () => {
+    if (getnow_f == false) {
+        getnow_f = true;
+        captureReq();
+        setTimeout(() => readReq(0, 0), 100);
+    }
+});
+
+ipcMain.on("isopen", (event, arg) => {
+    mainWindow.webContents.send('isopen', isopen);
+});
+ipcMain.on("connect", (event, arg) => {
+    console.log("open");
+    connect(ipaddr, port, recvip, recvport, recvData);
+});
+ipcMain.on("disconn", (event, arg) => {
+    console.log("close");
+});
